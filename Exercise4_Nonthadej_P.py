8d06abd7f9acd381651a00a2108a32ea878d7f4f
Novice-programmation <67594508+Novice-programmation@users.noreply.github.com>
2020-07-13 22:39:19 +0700

    Initial commit

---
+++ Exercise4_Nonthadej_P.py
@@ -0,0 +1,9 @@
+scorefe=90
+scoregb=91
+scoreics=92
+scorecp=93
+print("--- Your score ---")
+print("Foundation English :",scorefe)
+print("General Business :",scoregb)
+print("Introduction to Computer Systems :",scoreics)
+print("Computer Programming :",scorecp)
