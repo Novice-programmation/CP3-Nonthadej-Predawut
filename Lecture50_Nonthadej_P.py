41d9e72aa68b4d29ff06799b86d6e398e0693bdf
Novice-programmation <67594508+Novice-programmation@users.noreply.github.com>
2020-09-05 11:01:18 +0700

    Create Lecture50_Nonthadej_P.py

---
+++ Lecture50_Nonthadej_P.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def timeNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+addNumber(1, 2)
+minusNumber(1, 2)
+timeNumber(1, 2)
+divideNumber(1, 2)
